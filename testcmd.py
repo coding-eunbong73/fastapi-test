1ff2999db6e1cb402bc50313f16f022eb8925f2e
EunBong Lee <eunbong73.lee@gmail.com>
2022-11-07 13:59:48 +0900

    simul data change

--- testcmd.py
+++ testcmd.py
@@ -1,3 +1,26 @@
+import requests
+from requests.exceptions import HTTPError
+
+def jsontest (url='https://httpbin.org/get'):
+    try:
+        print(url)
+        response = requests.get(url)
+        #response.raise_for_status()
+        # access JSOn content
+        jsonResponse = response.json()
+        print("Entire JSON response")
+        print(jsonResponse)
+
+        print("JSON response[step_status]")
+        print(jsonResponse['step_status'])
+
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+
+
+
 def menu():
     print("MAIN MENU")
     print("-----------------")
@@ -14,3 +37,5 @@ def menu():
 menu_chosen = True
 choice = menu()
 print("You chose menu option", choice)
+if choice == 1 :
+    jsontest(url='http://localhost:8000/')
--- webjson.py
+++ webjson.py
@@ -9,12 +9,6 @@ async def read_root():
    ret_str = """{
    "deploy_type": "cluster",
    "status": "processing",
-  "step_status" : [
-     { "pre_setting": {
-             "start_time": "2020/09/20, 13:44:30"
-             "duration": 30
-           }
-     }
-     ]
+  "step_status" : "pre_setting\\nabc"
     }"""
    return Response(content=ret_str, media_type="application/json")
